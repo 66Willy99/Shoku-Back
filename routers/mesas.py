4c650454345a365f3998cface134a6acfb292c7d
Guillermo Adrian Mercado Castillo <133931931+66Willy99@users.noreply.github.com>
2025-05-17 22:25:59 -0400

    Implement CRUD operations for mesas in the MesaService and update the mesas router

--- routers/mesas.py
+++ routers/mesas.py
@@ -15,3 +15,41 @@ async def crear_mesa(
     service: MesaService = Depends(MesaService)
 ):
     return service.crear_mesa(user_id, restaurante_id, capacidad, estado, numero)
+
+@router.get("/all")
+async def obtener_mesas(
+    user_id:str= Body(...), 
+    restaurante_id:str = Body(...),
+    service: MesaService = Depends(MesaService)
+):
+    return service.obtener_mesas(user_id, restaurante_id)
+
+@router.get("/")
+async def obtener_mesa(
+    user_id:str= Body(...), 
+    restaurante_id:str = Body(...),
+    mesa_id:str = Body(...),
+    service: MesaService = Depends(MesaService)
+):
+    return service.obtener_mesa(user_id, restaurante_id, mesa_id)
+
+@router.put("/")
+async def actualizar_mesa(
+    user_id:str= Body(...), 
+    restaurante_id:str = Body(...),
+    mesa_id:str = Body(...),
+    capacidad:int = Body(...), 
+    estado:str = Body(...),
+    numero:int = Body(...),
+    service: MesaService = Depends(MesaService)
+):
+    return service.actualizar_mesa(user_id, restaurante_id, mesa_id, capacidad, estado, numero)
+
+@router.delete("/")
+async def eliminar_mesa(
+    user_id:str= Body(...), 
+    restaurante_id:str = Body(...),
+    mesa_id:str = Body(...),
+    service: MesaService = Depends(MesaService)
+):
+    return service.eliminar_mesa(user_id, restaurante_id, mesa_id)
